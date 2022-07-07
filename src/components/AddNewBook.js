93f75212762ab3fe2a0d892a8fe15f28b8c5eca9
Gabriel <gopxfs@hotmail.com>
2022-07-07 18:26:14 -0300

    Update add new book form
    Added a add book button and a title

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const AddNewBook = () => (
   <div className="AddNewBook">
-    <input type="text" placeholder="Book title" />
-    <input type="text" placeholder="Author" />
+    <h2>ADD NEW BOOK</h2>
+    <form>
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Author" />
+      <button type="submit">ADD BOOK</button>
+    </form>
   </div>
 );
 
