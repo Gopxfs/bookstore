a9614d85481e46145576418fb950d9ff193a1ae0
Gabriel <gopxfs@hotmail.com>
2022-07-12 21:06:04 -0300

    Add reducers name for easier access

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers(booksReducer, categoriesReducer);
+const rootReducer = combineReducers({ book: booksReducer, categories: categoriesReducer });
 const store = configureStore({
   reducer: rootReducer,
 });
