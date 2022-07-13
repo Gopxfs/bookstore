9031a475f9f5471e63da762530f02d1c84038ca5
Gabriel <gopxfs@hotmail.com>
2022-07-12 23:07:49 -0300

    Update configureStore syntax

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({ book: booksReducer, categories: categoriesReducer });
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    book: booksReducer, categories: categoriesReducer,
+  },
 });
 
 export default store;
