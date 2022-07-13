2198010ab0da85d4a59188394297af25429f5c13
Gabriel <gopxfs@hotmail.com>
2022-07-12 21:16:28 -0300

    Display hardcoded booklist

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useBooks } from '../redux/books/books';
 import Book from './Book';
 
-const Books = () => (
-  <>
-    <h1>Books</h1>
-    <Book title="Title" author="Author" />
-  </>
-);
+const Books = () => {
+  const bookList = useBooks();
+  const bookListDisplay = [];
+  for (let i = 0; i < bookList.length; i += 1) {
+    const newBook = <Book title={bookList[i].title} author={bookList[i].author} />;
+    bookListDisplay.push(newBook);
+  }
+  return (
+    <>
+      <h1>Books</h1>
+      <div>
+        {bookListDisplay}
+      </div>
+    </>
+  );
+};
 
 export default Books;
