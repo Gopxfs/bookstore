f3848f5ac4d8524d79ba21771ae5a6437c3f960e
Gabriel <gopxfs@hotmail.com>
2022-07-14 19:33:49 -0300

    Add class to BooksPage

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -2,10 +2,10 @@ import Books from '../components/Books';
 import AddNewBook from '../components/AddNewBook';
 
 const BooksPage = () => (
-  <>
+  <main>
     <Books />
     <AddNewBook />
-  </>
+  </main>
 );
 
 export default BooksPage;
